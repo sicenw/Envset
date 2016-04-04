cf63eb6308ad7870c885c4b45e3e2e4665b97f8b
wsicheng <sicheng@physics.ucsb.edu>
2016-04-04 13:48:30 -0700

    Moved to Emacs Prelude

--- .bash_aliases
+++ .bash_aliases
@@ -1,21 +1,28 @@
-alias lt='ls -ltr'
-      
-alias em='emacs -nw'
+alias ls='ls -G'
+alias lt='ls -ltrh'
+alias la='ls -a'
+alias lta='ls -ltrha'
 
+alias emacs='/Applications/Emacs.app/Contents/MacOS/Emacs -nw'
+alias em='/Applications/Emacs.app/Contents/MacOS/Emacs -nw -q --load ~/.emacs.d.backup/.emacs'
+
+alias dui='du -hc -d 1'
 alias ipynb='ipython notebook'
 
+alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
-alias .....='cd ../../../..'
-alias ......='cd ../../../../..'
-alias .......='cd ../../../../../..'
-alias ........='cd ../../../../../../..'
+alias ..3='cd ../../..'
+alias ..4='cd ../../../..'
+alias ..5='cd ../../../../..'
+alias ..6='cd ../../../../../..'
+alias ..7='cd ../../../../../../..'
 
 #Functional alias
 ei() {
     if [ -f "$1" ]
     then
-	emacs -nw "$1" --eval '(setq buffer-read-only t)'
+	emacs -q "$1" --eval '(setq buffer-read-only t)'
     else
 	echo "File $1 not found."
     fi
@@ -27,11 +34,14 @@ mkcd() {
 
 mkpdf(){
 
-    local FILENAME="$1" #| cut -d'.' --complement -f2-
-    FILENAME=$(echo $FILENAME | cut -d'.' --complement -f2-)
-    local LATEXFILENAME="$FILENAME.tex"
-    local PDFFILENAME="$FILENAME.pdf"
+    # local FILENAME="$1" #| cut -d'.' --complement -f2-
+    # FILENAME=$(echo $FILENAME | cut -d'.' --complement -f2-)
+    # local LATEXFILENAME="$FILENAME.tex"
+    # local PDFFILENAME="$FILENAME.pdf"
 
-    pdflatex $LATEXFILENAME && evince $PDFFILENAME 
+    local LATEXFILENAME="$1tex"
+    local PDFFILENAME="$1pdf"
+
+    pdflatex $LATEXFILENAME && open $PDFFILENAME 
 
 }
---
+++ custom.el
@@ -0,0 +1,101 @@
+;; --------------------------------------
+;; This file is based on Emacs Prelude at https://github.com/bbatsov/prelude
+;; To be put at ~/.emacs.d/personal/
+;; --------------------------------------
+
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier 'super)
+
+;; ---- Disable uncomfortable features ----
+;; (disable-theme 'zenburn)
+;; (setq prelude-theme 'solarized-dark)
+(setq prelude-whitespace nil)
+(setq prelude-flyspell nil)
+(setq electric-indent-mode nil)
+(setq prelude-auto-save nil)
+(global-set-key [remap move-beginning-of-line]
+                'move-beginning-of-line)
+
+
+;; ---- Wanted features ----
+(set-frame-parameter nil 'fullscreen 'fullboth)
+
+(when (not (display-graphic-p))
+  (menu-bar-mode -1)  
+  (global-hl-line-mode -1))
+(when  (display-graphic-p)
+  (setq confirm-kill-emacs 'yes-or-no-p)
+  (scroll-bar-mode -1))
+
+(setq display-time-day-and-date 't)
+(setq display-time-24hr-format 't)
+(display-time)
+
+;; ---- Additional functions ----
+(defun comment-dwim-line (&optional arg)
+  "Replacement for the comment-dwim command.
+   If no region is selected and current line is not blank and we are not at the end of the line,
+   then comment current line.
+   Replaces default behaviour of comment-dwim, when it inserts comment at the end of the line."
+  (interactive "*P")
+  (comment-normalize-vars)
+  (if (and (not (region-active-p)) (not (looking-at "[ \t]*$")))
+      (comment-or-uncomment-region (line-beginning-position) (line-end-position))
+    (comment-dwim arg)))
+
+;; ---- Add comfortable key bindings ----
+(global-set-key (kbd "C-\\")  'undo-tree-redo)
+(global-set-key (kbd "C-s-p") 'move-text-up)
+(global-set-key (kbd "C-s-n") 'move-text-down)
+(global-set-key (kbd "C-M-j") 'prelude-duplicate-current-line-or-region)
+(global-set-key (kbd "C-s-j") 'prelude-duplicate-and-comment-current-line-or-region)
+(global-set-key (kbd "s-SPC") 'just-one-space)
+(global-set-key (kbd "C-;")   'god-local-mode)
+
+;; ---- Key-chords ----
+(key-chord-define-global "OO" 'other-window)
+(key-chord-define-global "KK" 'delete-window)
+(key-chord-define-global "WW" 'delete-other-windows)
+(key-chord-define-global "BB" 'ido-switch-buffer) 
+(key-chord-define-global "LL" (lambda()
+                                (interactive)
+                                (split-window-right)
+                                (other-window 1)
+                                (prelude-switch-to-previous-buffer)))
+(key-chord-define-global "XX" (lambda()
+                                (interactive)
+                                (kill-buffer (current-buffer))
+                                (delete-window)))
+(key-chord-define-global ";;"  'comment-dwim-line)
+;; (key-chord-define latex-mode-map "{}"  "{}\C-b")
+
+;; ---- Multiple-cursors ----
+(require 'multiple-cursors)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-M-<mouse-1>") 'mc/add-cursor-on-click)
+
+;; ---- God-mode ----
+(require 'god-mode)
+(define-key god-local-mode-map (kbd ".") 'repeat)
+(define-key god-local-mode-map (kbd "i") 'god-local-mode)
+(require 'god-mode-isearch)
+(define-key isearch-mode-map (kbd "M-a") 'god-mode-isearch-activate)
+(define-key god-mode-isearch-map (kbd "M-a") 'god-mode-isearch-disable)
+(defun god-mode-update-cursor ()
+  (setq cursor-type (if (or god-local-mode buffer-read-only)
+                        '(bar . 3)
+                      'box)))
+(add-hook 'god-mode-enabled-hook 'god-mode-update-cursor)
+(add-hook 'god-mode-disabled-hook 'god-mode-update-cursor)
+
+
+;; --------------------------------------
+;;  File modification required features:
+;; --------------------------------------
+;; Mod: ~/.emacs.d/modules/prelude-c.el: (c-basic-offset 2)
+;; Add: ~/.emacs.d/modules/prelude-c.el: (local-unset-key (kbd "C-M-j"))
+;; Mod: ~/.emacs.d/elpa/smartparens/smartparens.el: ("C-s-s" . sp-splice-sexp)
+;; --------------------------------------
